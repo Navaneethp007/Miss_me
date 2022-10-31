ad010a03d1b7922640e2b483ef7825add3f1955b
chiragtyagi2003 <tyagichirag06@gmail.com>
2022-10-31 14:50:29 +0530

    improved welcome and forgot password page

--- miss_me/lib/main.dart
+++ miss_me/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:miss_me/pages/home.dart';
+import 'package:miss_me/pages/start.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: Splash(),
     );
   }
 }
